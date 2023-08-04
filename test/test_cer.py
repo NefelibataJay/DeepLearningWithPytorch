29eadd285875dd1cd6a4219ce30337ff509f2ff5
snoopy <snoopy_zj@163.com>
2023-08-04 17:28:30 +0800

    update

--- test/test_cer.py
+++ test/test_cer.py
@@ -2,13 +2,19 @@ from torchmetrics.text import CharErrorRate
 
 
 def test_cer():
-    cer = CharErrorRate(ignore_case=True, reduction="mean")
-    preds = ["this ", "there is an other"]
-    target = ["this ", "there is another"]
+    cer = CharErrorRate()
+    preds = ["什么辣鸡", "为什么1"]
+    target = ["什么垃圾", "为什么1"]
     cer(preds, target)
-    print(cer)
     print(cer.compute() * 100)
 
+    cer = CharErrorRate()
+    cer(preds[0], target[0])
+    print(cer.compute() * 100)
+
+    cer = CharErrorRate()
+    cer(preds[1], target[1])
+    print(cer.compute() * 100)
 
 if __name__ == "__main__":
     test_cer()
