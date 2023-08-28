7620018a8a86055ce67959f09801a283d8c93d68
KJay <snoopy_zj@163.com>
2023-08-28 21:57:24 +0800

    update cmvn

--- tool/compute_cmvn.py
+++ tool/compute_cmvn.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+import math
 import sys
 import os
 
 import argparse
 import json
 import codecs
+
+import numpy as np
 import yaml
 
 import torch
@@ -72,66 +75,37 @@ def compute_cmvn(configs):
 
     feat_dim = configs.dataset.extractor_conf.num_mel_bins
     resample_rate = configs.dataset.sample_rate
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='extract CMVN stats')
-    parser.add_argument('--num_workers',
-                        default=0,
-                        type=int,
-                        help='num of subprocess workers for processing')
-    parser.add_argument('--train_config',
-                        default='train_config.yaml',
-                        help='training yaml conf')
-    parser.add_argument('--in_scp', default="train/wav.scp", help='wav scp file')
-    parser.add_argument('--out_cmvn',
-                        default='train/global_cmvn',
-                        help='global cmvn file')
-
-    doc = "Print log after every log_interval audios are processed."
-    parser.add_argument("--log_interval", type=int, default=1000, help=doc)
-    args = parser.parse_args()
-
-    with open(args.train_config, 'r') as fin:
-        configs = yaml.load(fin, Loader=yaml.FullLoader)
-    feat_dim = configs['dataset_conf']['fbank_conf']['num_mel_bins']
-    resample_rate = 0
-    if 'resample_conf' in configs['dataset_conf']:
-        resample_rate = configs['dataset_conf']['resample_conf']['resample_rate']
-        print('using resample and new sample rate is {}'.format(resample_rate))
+    dataset_path = configs.dataset.dataset_path
+    manifests_path = configs.dataset.manifests_path
 
     collate_func = CollateFunc(feat_dim, resample_rate)
-    dataset = AudioDataset(args.in_scp)
+    dataset = AudioDataset(dataset_path, manifests_path)
     batch_size = 20
     data_loader = DataLoader(dataset,
                              batch_size=batch_size,
                              shuffle=True,
                              sampler=None,
-                             num_workers=args.num_workers,
                              collate_fn=collate_func)
-
     with torch.no_grad():
-        """
-        all_number  所有语音帧数
-        all_mean_stat  
-        all_var_stat  
-        """
-        all_number = 0
-        all_mean_stat = torch.zeros(feat_dim)
-        all_var_stat = torch.zeros(feat_dim)
-        wav_number = 0
+        num_frames = 0
+        means = torch.zeros(feat_dim)
+        variance = torch.zeros(feat_dim)
         for i, batch in enumerate(data_loader):
             number, mean_stat, var_stat = batch
-            all_mean_stat += mean_stat
-            all_var_stat += var_stat
-            all_number += number
-            wav_number += batch_size
+            means += mean_stat
+            variance += var_stat
+            num_frames += number
 
-    cmvn_info = {
-        'mean_stat': list(all_mean_stat.tolist()),
-        'var_stat': list(all_var_stat.tolist()),
-        'frame_num': all_number
-    }
+    for i in range(len(means)):
+        means[i] /= num_frames
+        variance[i] = variance[i] / num_frames - means[i] * means[i]
+        if variance[i] < 1.0e-20:
+            variance[i] = 1.0e-20
+        variance[i] = 1.0 / torch.sqrt(variance[i])
 
-    with open(args.out_cmvn, 'w') as fout:
-        fout.write(json.dumps(cmvn_info))
+    print(f"means: {means} --- variance: {variance}")
+
+
+
+if __name__ == "__main__":
+    pass
